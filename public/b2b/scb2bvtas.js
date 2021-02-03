abd580fc18eea7d370f3f6fdad45686c98ef939b
diegodps92 <77900383+diegodps92@users.noreply.github.com>
2021-02-03 17:48:11 -0500

    fix

--- public/b2b/scb2bvtas.js
+++ public/b2b/scb2bvtas.js
@@ -1,3 +1,12 @@
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+
+const elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
+});
 
 function search() {
      
---
+++ public/srcinicio.js
@@ -0,0 +1,9 @@
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+
+const elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
+});
